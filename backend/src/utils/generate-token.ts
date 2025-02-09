5dee41fa781cbba983f89fb3aa8110187bca9597
Abylai12 <abylai@Aigyerims-MacBook-Air.local>
2025-02-09 23:47:21 +0800

    cookie

--- backend/src/utils/generate-token.ts
+++ backend/src/utils/generate-token.ts
@@ -9,8 +9,8 @@ export const generateToken = (userId: string, res: Response) => {
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000, // MS
     httpOnly: true, // prevent XSS attacks cross-site scripting attacks
-    sameSite: "none", // CSRF attacks cross-site request forgery attacks
-    secure: true,
+    sameSite: "lax", // CSRF attacks cross-site request forgery attacks
+    secure: false,
   });
 
   return token;
