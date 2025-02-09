0488137ccb36b0e5b77893c2189207b4fee1bc92
Abylai12 <abylai@Aigyerims-MacBook-Air.local>
2025-02-10 00:05:25 +0800

    same site lax secure true

--- backend/src/utils/generate-token.ts
+++ backend/src/utils/generate-token.ts
@@ -10,7 +10,7 @@ export const generateToken = (userId: string, res: Response) => {
     maxAge: 7 * 24 * 60 * 60 * 1000, // MS
     httpOnly: true, // prevent XSS attacks cross-site scripting attacks
     sameSite: "lax", // CSRF attacks cross-site request forgery attacks
-    secure: false,
+    secure: true,
   });
 
   return token;
